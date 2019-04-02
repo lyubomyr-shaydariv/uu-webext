439b9d39eca8a0666efe3780a94c21966ff8ca26
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2019-04-02 22:17:00 +0300

    Added an example Evernote support

--- src/background.js
+++ src/background.js
@@ -17,6 +17,9 @@ const filter = {
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
 	const url = new URL(details.url);
 	let redirectUrl = null;
+	if ( url.hostname === "www.evernote.com" && url.pathname === "/OutboundRedirect.action" && url.searchParams.get("dest") ) {
+		redirectUrl = url.searchParams.get("dest");
+	}
 	if ( redirectUrl ) {
 		return { redirectUrl };
 	}
