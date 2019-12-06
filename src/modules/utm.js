5a76e5abb96d4d9f432467545b8e655c198981c8
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2019-12-06 10:50:22 +0200

    Added utm_nooverride to the UTM checker

--- src/modules/utm.js
+++ src/modules/utm.js
@@ -1,6 +1,6 @@
 registerModule(function() {
 	function isSafeKeyPair(k, v) {
-		return k !== "utm_campaign" && k !== "utm_cid" && k !== "utm_content" && k !== "utm_medium" && k !== "utm_name" && k !== "utm_reader" && k !== "utm_referrer" && k !== "utm_source" && k !== "utm_term";
+		return k !== "utm_campaign" && k !== "utm_cid" && k !== "utm_content" && k !== "utm_medium" && k !== "utm_name" && k != "utm_nooverride" && k !== "utm_reader" && k !== "utm_referrer" && k !== "utm_source" && k !== "utm_term";
 	};
 	return {
 		redirect: function(url) {
