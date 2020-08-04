9d528d48d0f1740f51e79f47711eb15d1e2f7386
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2020-08-04 11:19:51 +0300

    Improved Mandrill support

--- src/modules/mandrill.js
+++ src/modules/mandrill.js
@@ -2,7 +2,11 @@ registerModule(function() {
 	return {
 		redirect: function(url) {
 			if ( url.hostname === "mandrillapp.com" && url.pathname.startsWith("/track/click/") ) {
-				return getRedirectToWarningPage(url);
+				try {
+					return new URL(JSON.parse(JSON.parse(atob(url.searchParams.get("p"))).p).url);
+				} catch ( ignored ) {
+					return getRedirectToWarningPage(url);
+				}
 			}
 		}
 	};
