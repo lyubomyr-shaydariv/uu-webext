a8060582dd7a38cce0b9b9e8a12edbdcfbd20f3f
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2021-07-25 22:40:40 +0300

    Removed favicons.lst

--- favicons/update.sh
+++ favicons/update.sh
@@ -5,6 +5,7 @@ set -e
 BASE_DIR="$(dirname "$0")"
 
 while read -r DOMAIN; do
+	DOMAIN="${DOMAIN%.*}"
 	echo -n "processing $DOMAIN... "
 	curl --silent "https://www.google.com/s2/favicons?domain=$DOMAIN&sz=16" > "$BASE_DIR/$DOMAIN"
 	case "$(file "$BASE_DIR/$DOMAIN")" in
@@ -17,5 +18,5 @@ while read -r DOMAIN; do
 		;;
 	esac
 	echo 'OK'
-done < "$BASE_DIR/favicons.lst"
+done < <(find "$BASE_DIR" -mindepth 1 -maxdepth 1 -type f -name '*.png' -printf "%f\n")
 echo 'done!'
