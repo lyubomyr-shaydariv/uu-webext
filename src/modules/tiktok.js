9acea3060516180d674673746c684447effa26d2
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-05-01 10:15:08 +0300

    Added TikTok support

---
+++ src/modules/tiktok.js
@@ -0,0 +1,13 @@
+registerModule(function() {
+	function isSafeKeyPair(k, v) {
+		return k !== "_d" && k !== "checksum" && k !== "is_copy_url" && k !== "is_from_webapp" && k !== "language" && k !== "preview_pb" && k !== "sec_user_id" && k !== "sender_device" && k !== "sender_web_id" && k !== "share_app_id" && k !== "share_link_id" && k !== "share_item_id" && k !== "source" && k !== "timestamp" && k !== "tt_from" && k !== "u_code" && k !== "user_id";
+	};
+	return {
+		redirect: function(url) {
+			if ( url.hostname === "tiktok.com" || url.hostname.endsWith(".tiktok.com") ) {
+				url.search = removeSearchPair(url.search, isSafeKeyPair);
+				url.hash = removeHashPair(url.hash, isSafeKeyPair);
+			}
+		}
+	};
+});
