a12ebc3c8af639efed77043e40eace1b0477f2e3
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-05-01 09:17:13 +0300

    Improved Facebook domain support

--- src/modules/facebook.js
+++ src/modules/facebook.js
@@ -9,6 +9,19 @@ registerModule(function() {
 });
 registerModule(function() {
 	function isSafeKeyPair(k, v) {
+		return k !== "hrc" && k !== "refsrc";
+	};
+	return {
+		redirect: function(url) {
+			if ( url.hostname === "www.facebook.com" || url.hostname === "facebook.com" ) {
+				url.search = removeSearchPair(url.search, isSafeKeyPair);
+				url.hash = removeHashPair(url.hash, isSafeKeyPair);
+			}
+		}
+	};
+});
+registerModule(function() {
+	function isSafeKeyPair(k, v) {
 		return k !== "fbclid" && k !== "fb_action_ids" && k !== "fb_action_types" && k !== "fb_ref" && k !== "fb_source";
 	};
 	return {
