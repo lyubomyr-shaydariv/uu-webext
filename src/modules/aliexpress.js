b14cb00d3b2cd4bf2c3097a074bc3a2bf1db0d8e
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-05-01 08:59:41 +0300

    Improved AliExpress domain support

--- src/modules/aliexpress.js
+++ src/modules/aliexpress.js
@@ -1,5 +1,19 @@
 registerModule(function() {
 	function isSafeKeyPair(k, v) {
+		return k !== "algo_expid" && k !== "algo_pvid" && k !== "btsid" && k !== "expid" && k !== "initiative_id" && k !== "scm_id" && k !== "spm" && k !== "ws_ab_test";
+	};
+	const domainRx = /^(?:[^.]+\.)?aliexpress\.[^.]+$/;
+	return {
+		redirect: function(url) {
+			if ( domainRx.test(url.hostname) ) {
+				url.search = removeSearchPair(url.search, isSafeKeyPair);
+				url.hash = removeHashPair(url.hash, isSafeKeyPair);
+			}
+		}
+	};
+});
+registerModule(function() {
+	function isSafeKeyPair(k, v) {
 		return k !== "aff_platform" && k !== "aff_trace_key";
 	};
 	return {
