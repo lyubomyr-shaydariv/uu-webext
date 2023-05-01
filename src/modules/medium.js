363d4118764a918c013aa22d0a09b77fef354637
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-05-01 09:06:36 +0300

    Improved Medium domain support

--- src/modules/medium.js
+++ src/modules/medium.js
@@ -1,4 +1,17 @@
 registerModule(function() {
+	function isSafeKeyPair(k, v) {
+		return k !== "_branch_match_id" && k !== "source";
+	};
+	return {
+		redirect: function(url) {
+			if ( url.hostname === "medium.com" ) {
+				url.search = removeSearchPair(url.search, isSafeKeyPair);
+				url.hash = removeHashPair(url.hash, isSafeKeyPair);
+			}
+		}
+	};
+});
+registerModule(function() {
 	return {
 		redirect: function(url) {
 			if ( url.hostname === "medium.com" && url.pathname === "/r/" ) {
