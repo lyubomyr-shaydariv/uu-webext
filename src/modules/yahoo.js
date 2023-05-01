fb3698e2457b4af5c2c2f80c4a83d0fd6d04c738
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-05-01 09:04:44 +0300

    Improved Yahoo domain support

--- src/modules/yahoo.js
+++ src/modules/yahoo.js
@@ -1,5 +1,19 @@
 registerModule(function() {
 	function isSafeKeyPair(k, v) {
+		return k !== "guccounter";
+	};
+	const domainRx = /^(?:[^.]+\.)?yahoo\.[^.]+$/;
+	return {
+		redirect: function(url) {
+			if ( domainRx.test(url.hostname) ) {
+				url.search = removeSearchPair(url.search, isSafeKeyPair);
+				url.hash = removeHashPair(url.hash, isSafeKeyPair);
+			}
+		}
+	};
+});
+registerModule(function() {
+	function isSafeKeyPair(k, v) {
 		return k !== "soc_src" && k !== "soc_trk";
 	};
 	return {
