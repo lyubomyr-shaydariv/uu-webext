af0b1cf57a1eb7d17f4bea50133ea4b316cc3e93
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-05-01 08:41:25 +0300

    Improved Yandex domain support

--- src/modules/yandex.js
+++ src/modules/yandex.js
@@ -1,5 +1,19 @@
 registerModule(function() {
 	function isSafeKeyPair(k, v) {
+		return k !== "lr" && k !== "redircnt";
+	}
+	const domainRx = /^(?:[^.]+\.)?yandex\.[^.]+$/;
+	return {
+		redirect: function(url) {
+			if ( domainRx.test(url.hostname) ) {
+				url.search = removeSearchPair(url.search, isSafeKeyPair);
+				url.hash = removeHashPair(url.hash, isSafeKeyPair);
+			}
+		}
+	};
+});
+registerModule(function() {
+	function isSafeKeyPair(k, v) {
 		return k !== "yclid" && k !== "_openstat";
 	};
 	return {
