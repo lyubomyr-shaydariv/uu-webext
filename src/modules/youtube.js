e7b9a69819531f92df84a15a280eb7c7e6d5fc7c
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-11-08 14:08:34 +0200

    Added clean youtu.be redirect

--- src/modules/youtube.js
+++ src/modules/youtube.js
@@ -9,6 +9,16 @@ addRule((function() {
 	};
 })());
 addRule((function() {
+	const filter = createFilterByConstantKeys("si");
+	return {
+		redirect: function(url) {
+			if ( url.hostname === "youtu.be" ) {
+				cleanSearchAndHashPairs(url, filter);
+			}
+		}
+	};
+})());
+addRule((function() {
 	return {
 		redirect: function(url) {
 			if ( (url.hostname === "youtube.com" || url.hostname.endsWith(".youtube.com")) && url.pathname === "/redirect" ) {
