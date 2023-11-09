e2ee2e7ad5cd5387596b59243cbaf1f74a3a154c
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-11-09 13:08:07 +0200

    Improved Bing support

--- src/modules/bing.js
+++ src/modules/bing.js
@@ -1,5 +1,5 @@
 addRule((function() {
-	const filter = createFilterByConstantKeys("cvid", "form", "pq", "qs", "sc", "sk", "sp");
+	const filter = createFilterByConstantKeys("cvid", "form", "pq", "qs", "qp", "sc", "sk", "sp");
 	return {
 		redirect: function(url) {
 			if ( url.hostname === "bing.com" || url.hostname.endsWith(".bing.com") ) {
