0892705b9e1884bcd9b06e25f9f454c88b1cd42c
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-11-09 12:55:43 +0200

    Added Reddit tracking parameters support

--- src/modules/reddit.js
+++ src/modules/reddit.js
@@ -1,4 +1,14 @@
 addRule((function() {
+	const filter = createFilterByConstantKeys("$3p", "$deep_link", "$original_link", "_branch_match_id", "correlation_id", "ref_campaign", "ref_source");
+	return {
+		redirect: function(url) {
+			if ( url.hostname === "reddit.com" || url.hostname.endsWith(".reddit.com") ) {
+				cleanSearchAndHashPairs(url, filter);
+			}
+		}
+	};
+})());
+addRule((function() {
 	return {
 		redirect: function(url) {
 			if ( (url.hostname === "out.reddit.com") && /^\/[^/]+$/.test(url.pathname) ) {
