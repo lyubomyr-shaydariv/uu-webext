58fa2f2dc20b878fd178f2d02966d73b33066c74
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-11-23 00:38:22 +0200

    Blocked confirmation warning page refactoring

--- src/warn.js
+++ src/warn.js
@@ -1,11 +1,7 @@
-(function() {
-
-	window.onload = function() {
-		const searchParams = new URLSearchParams(document.location.search);
-		const url = decodeURIComponent(searchParams.get("url"));
-		const urlElement = document.getElementById("url");
-		urlElement.setAttribute("href", url);
-		urlElement.innerText = url;
-	};
-
-})();
+window.addEventListener("load", () => {
+	const searchParams = new URLSearchParams(document.location.search);
+	const url = decodeURIComponent(searchParams.get("url"));
+	const urlElement = document.getElementById("url");
+	urlElement.setAttribute("href", url);
+	urlElement.innerText = url;
+});
