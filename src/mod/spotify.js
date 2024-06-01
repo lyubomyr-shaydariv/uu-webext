2a7fb4ce0ec7de3d51b714c8a1d3b102ae6573ea
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-01 12:38:48 +0300

    Minor changes to the Spotify rules

--- src/mod/spotify.js
+++ src/mod/spotify.js
@@ -3,6 +3,6 @@ import { AT, JUST, RULE } from '/rules.js';
 export default [
 	RULE.MUTATE_ENTRIES_AT(
 		JUST.EXCLUDING('context', 'si'),
-		AT.HOSTNAME('open.spotify.com')
+		AT.DOMAIN('spotify.com')
 	)
 ];
