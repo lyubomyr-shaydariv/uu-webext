5c7f3cff8021e211988ad20273d2ef6e2b948b67
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-01 13:35:50 +0300

    Removed the warn-redirection page

--- src/warn.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-	const searchParams = new URLSearchParams(document.location.search);
-	const url = decodeURIComponent(searchParams.get('url'));
-	const urlElement = document.getElementById('url');
-	urlElement.setAttribute('href', url);
-	urlElement.innerText = url;
-});
