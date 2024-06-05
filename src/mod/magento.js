7e30e5a543bef9bb4d8adeb7eab246be3e98015d
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-05 20:47:12 +0300

    Removed the Magento module since it is already superceded by the itm_ module

--- src/mod/magento.js
+++
@@ -1,8 +0,0 @@
-import { AT, JUST, RULE } from '/rules.js';
-
-export default [
-	RULE.MUTATE_ENTRIES_AT(
-		JUST.EXCLUDING('itm_campaign', 'itm_medium', 'itm_source', 'itm_term'),
-		AT.DOMAIN('magento.com')
-	)
-];
