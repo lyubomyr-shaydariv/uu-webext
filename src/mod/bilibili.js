4484cfddb04693870643440b4078f5deefcd24ff
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-09 19:28:58 +0300

    Upgraded Bilibili support

--- src/mod/bilibili.js
+++ src/mod/bilibili.js
@@ -4,5 +4,13 @@ export default [
 	RULE()
 		.AT().DOMAIN('bilibili.com')
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('callback', 'spm_id_from')
+		.DO().REMOVE('callback', 'spm_id_from'),
+	RULE()
+		.AT().DOMAIN('live.bilibili.com')
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('broadcast_type', 'is_room_feed', 'session_id', 'visit_id'),
+	RULE()
+		.AT().DOMAIN('m.bilibili.com')
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('bbid', 'ts')
 ];
