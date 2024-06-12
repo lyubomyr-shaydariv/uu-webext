daa7ce0f574db5d68f6ea90e9752bf6b1d24cd66
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-12 19:14:21 +0300

    Improved Bing support

--- src/mod/bing.js
+++ src/mod/bing.js
@@ -4,5 +4,10 @@ export default [
 	RULE()
 		.AT().DOMAIN('bing.com')
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('cvid', 'form', 'pq', 'qs', 'qp', 'sc', 'sk', 'sp')
+		.DO().REMOVE('cvid', 'form', 'pq', 'qs', 'qp', 'sc', 'sk', 'sp'),
+	RULE()
+		.AT().DOMAIN('edgeservices.bing.com').PATHNAME('/edgesvc/redirect')
+		.FROM().QUERY_ENTRIES()
+		.APPLY().GET_PROPERTY('url').TO_URL()
+		.DO().REDIRECT()
 ];
