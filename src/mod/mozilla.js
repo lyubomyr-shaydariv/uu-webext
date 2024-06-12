69af5df65b70de355588e6aeab9f1531f5be61fc
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-12 19:58:08 +0300

    Improved Mozilla support

--- src/mod/mozilla.js
+++ src/mod/mozilla.js
@@ -2,7 +2,7 @@ import { RULE } from '/rules.js';
 
 export default [
 	RULE()
-		.AT().HOSTNAME('prod.outgoing.prod.webservices.mozgcp.net')
+		.AT().HOSTNAME('outgoing.prod.mozaws.net', 'prod.outgoing.prod.webservices.mozgcp.net')
 		.FROM().PATHNAME()
 		.APPLY().FROM_URI_COMPONENT().EXECUTE_REGEXP(/^\/v[^/]+\/[0-9a-f]+\/(.*)$/).GET_PROPERTY(1).TO_URL()
 		.DO().REDIRECT()
