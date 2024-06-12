288c598a718173e9f849445bcf37682e9fe6e9a5
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-12 13:36:03 +0300

    Improved Outlook support

--- src/mod/outlook.js
+++ src/mod/outlook.js
@@ -2,7 +2,7 @@ import { RULE } from '/rules.js';
 
 export default [
 	RULE()
-		.AT().HOSTNAME('safelinks.protection.outlook.com')
+		.AT().DOMAIN('safelinks.protection.outlook.com')
 		.FROM().QUERY_ENTRIES()
 		.APPLY().GET_PROPERTY('url').TO_URL()
 		.DO().REDIRECT()
