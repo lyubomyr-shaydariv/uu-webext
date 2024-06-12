d9a79f82eb3dec1fa394d9899fee5fabaa956805
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-12 14:14:13 +0300

    Improved GitHub support

--- src/mod/github.js
+++ src/mod/github.js
@@ -4,5 +4,10 @@ export default [
 	RULE()
 		.AT().DOMAIN('github.com')
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('email_source', 'email_token')
+		.DO().REMOVE('email_source', 'email_token'),
+	RULE()
+		.AT().DOMAIN('github-redirect.dependabot.com')
+		.FROM().PATHNAME()
+		.APPLY().REPLACE_STRING('/', 'https://github.com/').TO_URL()
+		.DO().REDIRECT()
 ];
