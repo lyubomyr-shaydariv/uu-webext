5011e8d2f4e2673dbe47a26615a1322efb837390
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-14 11:25:38 +0300

    Improved Adobe support

--- src/mod/adobe.js
+++ src/mod/adobe.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('mkt_tok', 'sc_cid', 's_cid')
+		.DO().REMOVE('ICID', 'icid', 'mkt_tok', 'sc_cid', 's_cid')
 ];
