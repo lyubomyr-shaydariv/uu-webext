08172882a6cb27adac38965b36ae791ac1cbcead
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-15 13:23:05 +0300

    Fixed eBay domain levels

--- src/mod/ebay.js
+++ src/mod/ebay.js
@@ -2,7 +2,7 @@ import { RULE } from '/rules.js';
 
 export default [
 	RULE()
-		.AT().HOSTNAME(/^(?:[^.]+\.)?ebay\.[^.]+$/)
+		.AT().HOSTNAME(/^(?:[^.]+\.)?ebay(?:\.[a-z]+)?\.[a-z]+$/)
 		.FROM().QUERY_ENTRIES()
 		.DO().REMOVE('_from', '_trkparms', '_trksid', 'amdata', 'epid', 'hash', 'var')
 ];
