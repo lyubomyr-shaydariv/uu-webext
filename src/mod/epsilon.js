2a4a0052dcd9f9a67e76983eef66f7b97e37a6ab
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-15 12:39:01 +0300

    Fixed/loosened the Epsilon/dotomi.com path regexp

--- src/mod/epsilon.js
+++ src/mod/epsilon.js
@@ -4,6 +4,6 @@ export default [
 	RULE()
 		.AT().DOMAIN('cj.dotomi.com').PATHNAME(/^\/links-t\//)
 		.FROM().PATHNAME()
-		.APPLY().EXECUTE_REGEXP(/^\/links-t\/\d+\/\w+\/\w+\/\w+\/\w+_[0-9a-f]{24}\/(.*)/).GET_PROPERTY(1).TO_URL()
+		.APPLY().EXECUTE_REGEXP(/^\/links-t\/\d+\/\w+\/\w+\/\w+\/\w+\/(.*)/).GET_PROPERTY(1).TO_URL()
 		.DO().REDIRECT()
 ];
