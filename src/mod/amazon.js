d88bba4286570c72e02a4f09785372b0e922f02f
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-16 11:02:48 +0300

    Improved Amazon support

--- src/mod/amazon.js
+++ src/mod/amazon.js
@@ -8,7 +8,7 @@ export default [
 	RULE()
 		.AT().HOSTNAME(/^(www\.)?amazon.(ac|ca|cn|com|co\.jp|co\.uk|de|eg|es|fr|in|it|nl|pl|sg)$/)
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('_encoding', 'aaxitk', 'ascsubtag', 'camp', 'content-id', 'creative', 'creativeASIN', 'crid', 'dchild', 'field-lbr_brands_browse-bin', 'hsa_cr_id', 'keywords', 'linkCode', 'ms3_c', 'pd_rd_r', 'pd_rd_w', 'pd_rd_wg', 'pf_rd_i', 'pf_rd_m', 'pf_rd_p', 'pf_rd_r', 'pf_rd_s', 'pf_rd_t', 'pf', 'pf_rd_i', 'pf_rd_m', 'pf_rd_p', 'pf_rd_r', 'pf_rd_s', 'pf_rd_t', 'psc', 'qid', 'qualifier', 'ref_', 'refRID', 'rnid', 's', 'smid', 'spIA', 'sprefix', 'sr', 'tag', 'th'),
+		.DO().REMOVE('_encoding', 'aaxitk', 'ascsubtag', 'camp', 'content-id', 'creative', 'creativeASIN', 'crid', 'dchild', 'field-lbr_brands_browse-bin', 'hsa_cr_id', 'keywords', 'linkCode', 'linkId', 'ms3_c', 'pd_rd_r', 'pd_rd_w', 'pd_rd_wg', 'pf_rd_i', 'pf_rd_m', 'pf_rd_p', 'pf_rd_r', 'pf_rd_s', 'pf_rd_t', 'pf', 'pf_rd_i', 'pf_rd_m', 'pf_rd_p', 'pf_rd_r', 'pf_rd_s', 'pf_rd_t', 'psc', 'qid', 'qualifier', 'ref_', 'refRID', 'rnid', 's', 'smid', 'spIA', 'sprefix', 'sr', 'tag', 'th'),
 	RULE()
 		.AT().HOSTNAME(/^(www\.)?amazon.(ac|ca|cn|com|co\.jp|co\.uk|de|eg|es|fr|in|it|nl|pl|sg)$/).PATHNAME('/gp/redirect.html')
 		.FROM().QUERY_ENTRIES()
