69f3c0588ee6c3a92a639ca53e99a0b5322f78ee
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-17 16:05:02 +0300

    Improved MailerLite support

--- src/mod/mailerlite.js
+++ src/mod/mailerlite.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('ml_subscriber', 'ml_subscriber_hash')
+		.DO().REMOVE('ml_sub', 'ml_sub_hash', 'ml_subscriber', 'ml_subscriber_hash')
 ];
