65f3fd4605511b0602e2d04de0607357329242be
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-17 14:08:38 +0300

    Improved Epic Games support

--- src/mod/epicgames.js
+++ src/mod/epicgames.js
@@ -2,6 +2,10 @@ import { RULE } from '/rules.js';
 
 export default [
 	RULE()
+		.AT().DOMAIN('epicgames.com')
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('epic_affiliate', 'epic_gameId'),
+	RULE()
 		.AT().DOMAIN('redirect.epicgames.com')
 		.FROM().QUERY_ENTRIES()
 		.APPLY().GET_PROPERTY('redirectTo').TO_URL()
