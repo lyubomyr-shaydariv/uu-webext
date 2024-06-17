9f478bcd595fc9fca924fbcad87cde38b7023f13
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-17 13:58:08 +0300

    Improved Alibaba support

--- src/mod/alibaba.js
+++ src/mod/alibaba.js
@@ -4,5 +4,9 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('spm', 'spmA', 'spmB', 'spmC', 'spmD', 'spmE')
+		.DO().REMOVE('spm', 'spmA', 'spmB', 'spmC', 'spmD', 'spmE'),
+	RULE()
+		.AT().HOSTNAME('arms-retcode.aliyuncs.com')
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('_v', 'api', 'begin', 'behavior', 'c2', 'c3', 'code', 'ct', 'dl', 'enableLinkTrace', 'enableSPA', 'environment', 'flag', 'msg', 'page', 'pid', 'post_res', 'pv_id', 'release', 'sample', 'sampling', 'sr', 'success', 'tag', 'traceId', 'uid', 'vp')
 ];
