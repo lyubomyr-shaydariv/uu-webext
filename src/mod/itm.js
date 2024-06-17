4bb2d68c3bfa920a2a0310d8f704b990a1d61984
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-17 15:37:56 +0300

    The itm_ prefix now covers all query parameters starting with the prefix

--- src/mod/itm.js
+++ src/mod/itm.js
@@ -1,9 +1,8 @@
 import { RULE } from '/rules.js';
 
-// This is Magento stuff
 export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('itm_campaign', 'itm_content', 'itm_medium', 'itm_source', 'itm_term')
+		.DO().REMOVE(/^itm_.*/)
 ];
