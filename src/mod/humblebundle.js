6efca70a3a1a064d5107fdb2fb5b782185cffe21
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-17 14:12:25 +0300

    Improved Humble Bundle support

--- src/mod/humblebundle.js
+++ src/mod/humblebundle.js
@@ -4,5 +4,9 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('hmb_campaign', 'hmb_medium', 'hmb_source')
+		.DO().REMOVE('hmb_campaign', 'hmb_medium', 'hmb_source'),
+	RULE()
+		.AT().DOMAIN('humblebundle.com')
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('partner')
 ];
