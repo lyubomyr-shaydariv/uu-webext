91e030d446fbd3dab726e40b4ab9bd3b8f50e0e8
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-17 14:41:04 +0300

    Added redirect_log_mongo_id and redirect_mongo_id tracking parameters (is it related to MongoDB?)

--- src/mod/_.js
+++ src/mod/_.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('c_id', 'campaign_id', 'cmpid', 'gdftrk', 'hc_location', 'mbid', 'ncid', 'nr_email_referer', 'rb_clickid', 'rc_fifo', 'rc_col', 'trkid', 'ymid', /^trk_.*$/)
+		.DO().REMOVE('c_id', 'campaign_id', 'cmpid', 'gdftrk', 'hc_location', 'mbid', 'ncid', 'nr_email_referer', 'rb_clickid', 'rc_fifo', 'rc_col', 'redirect_log_mongo_id', 'redirect_mongo_id', 'trkid', 'ymid', /^trk_.*$/)
 ];
