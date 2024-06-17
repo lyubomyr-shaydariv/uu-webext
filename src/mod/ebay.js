5bf3a1b916980ab7078352ff0c4280dbd002173a
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-17 16:58:20 +0300

    Improved eBay support

--- src/mod/ebay.js
+++ src/mod/ebay.js
@@ -4,5 +4,9 @@ export default [
 	RULE()
 		.AT().HOSTNAME(/^(?:[^.]+\.)?ebay(?:\.[a-z]+)?\.[a-z]+$/)
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('_from', '_trkparms', '_trksid', 'amdata', 'epid', 'hash', 'var')
+		.DO().REMOVE('_from', '_trkparms', '_trksid', 'amdata', 'epid', 'hash', 'var'),
+	RULE()
+		.AT().HOSTNAME(/^(?:[^.]+\.)?ebay(?:\.[a-z]+)?\.[a-z]+$/).PATHNAME(/^\/itm\/.*/)
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE(/^.*$/)
 ];
