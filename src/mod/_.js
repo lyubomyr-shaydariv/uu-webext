0c8846b2dd13f820182f187d6dc943c018827e7a
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-18 18:07:30 +0300

    Added ex_cid tracking parameter support

--- src/mod/_.js
+++ src/mod/_.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('c_id', 'campaign_id', 'cm_cr', 'cm_mmc', 'cm_me', 'cm_re', 'cm_sp', /^cm_mmca\d+$/, 'cmpid', 'gdftrk', 'hc_location', 'mbid', 'ncid', 'nr_email_referer', 'rb_clickid', 'rc_fifo', 'rc_col', 'redirect_log_mongo_id', 'redirect_mongo_id', 'ss_campaign_id', 'ss_campaign_name', 'ss_campaign_sent_date', 'ss_source', 'trkid', 'ymid', /^trk_.*$/)
+		.DO().REMOVE('c_id', 'campaign_id', 'cm_cr', 'cm_mmc', 'cm_me', 'cm_re', 'cm_sp', /^cm_mmca\d+$/, 'cmpid', 'ex_cid', 'gdftrk', 'hc_location', 'mbid', 'ncid', 'nr_email_referer', 'rb_clickid', 'rc_fifo', 'rc_col', 'redirect_log_mongo_id', 'redirect_mongo_id', 'ss_campaign_id', 'ss_campaign_name', 'ss_campaign_sent_date', 'ss_source', 'trkid', 'ymid', /^trk_.*$/)
 ];
