2b68f8596d753ce211de7cd563d8b28fdc9044cf
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-18 21:32:01 +0300

    Improved Bilibili support

--- src/mod/bilibili.js
+++ src/mod/bilibili.js
@@ -2,9 +2,9 @@ import { RULE } from '/rules.js';
 
 export default [
 	RULE()
-		.AT().DOMAIN('bilibili.com')
+		.AT().DOMAIN('bilibili.com').EXCEPT('api.bilibili.com', 'space.bilibili.com')
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('callback', 'spm_id_from'),
+		.DO().REMOVE('buvid', 'callback', 'from', 'from_source', 'is_story_h5', 'mid', 'msource', 'plat_id', 'refer_from', 'seid', 'share_from', 'share_medium', 'share_plat', 'share_session_id', 'share_source', 'share_tag', 'spm_id_from', 'timestamp', 'unique_k', 'up_id', 'vd_source'),
 	RULE()
 		.AT().DOMAIN('live.bilibili.com')
 		.FROM().QUERY_ENTRIES()
