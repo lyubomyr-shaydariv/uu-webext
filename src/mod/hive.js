2e519215663cbe15f013a2369ae1a6007fc4070d
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-18 20:17:11 +0300

    Improved Hive support

--- src/mod/hive.js
+++ src/mod/hive.js
@@ -2,6 +2,10 @@ import { RULE } from '/rules.js';
 
 export default [
 	RULE()
+		.AT().DOMAIN().EXCEPT('app.hive.com')
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('h_sid', 'h_slt'),
+	RULE()
 		.AT().DOMAIN('app.hive.co').PATHNAME(/^\/email\//)
 		.FROM().QUERY_ENTRIES()
 		.DO().REMOVE('h_sid', 'h_slt')
