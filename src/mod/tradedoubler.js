ca4bc92db945a3c99cd2204df75f81872584c60f
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-18 23:12:28 +0300

    Improved Tradedoubler support

--- src/mod/tradedoubler.js
+++ src/mod/tradedoubler.js
@@ -4,6 +4,6 @@ export default [
 	RULE()
 		.AT().DOMAIN('tradedoubler.com').PATHNAME('/click')
 		.FROM().QUERY_ENTRIES()
-		.APPLY().GET_PROPERTY('url').TO_URL()
+		.APPLY().GET_PROPERTY('_td_deeplink', 'url').TO_URL()
 		.DO().REDIRECT()
 ];
