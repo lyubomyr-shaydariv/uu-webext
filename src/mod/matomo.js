2d6a7a963a9b5127dc8caee79ffe3b7ce2a08cdd
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-18 21:19:56 +0300

    Improved Matomo and Piwik support

--- src/mod/matomo.js
+++ src/mod/matomo.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE(/^mtm_.*/, /^pk_.*/, 'piwik_campaign', 'piwik_kwd')
+		.DO().REMOVE('matomo_campaign', 'matomo_cid', 'matomo_content', 'matomo_group', 'matomo_keyword', 'matomo_medium', 'matomo_placement', 'matomo_source', /^mtm_.*/, /^pk_.*/, 'piwik_campaign', 'piwik_keyword', 'piwik_kwd')
 ];
