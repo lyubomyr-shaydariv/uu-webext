3e9e783da6a098e3e1126de159f2fc13e1e640c5
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-19 15:54:41 +0300

    Improved Adobe (ColdFusion) support

--- src/mod/adobe.js
+++ src/mod/adobe.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('ef_id', 'ICID', 'icid', 'mkt_tok', 's_campaign', 's_cid', 's_kwcid')
+		.DO().REMOVE('CFID', 'CFTOKEN', 'ef_id', 'ICID', 'icid', 'mkt_tok', 's_campaign', 's_cid', 's_kwcid')
 ];
