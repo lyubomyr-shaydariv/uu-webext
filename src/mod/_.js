dc95016217a87c81eaca8e77bbc5e404bad5f2c1
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-19 15:21:06 +0300

    Added _cmp, _ebid, _ehid, ehid, _mid, and pmcode tracking parameters support

--- src/mod/_.js
+++ src/mod/_.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('_bta_tid', '_bta_c', 'c_id', 'campaign_id', 'cm_cr', 'cm_mmc', 'cm_me', 'cm_re', 'cm_sp', /^cm_mmca\d+$/, 'cmpid', 'ex_cid', 'gdftrk', 'hc_location', 'mbid', 'ncid', 'nr_email_referer', 'rb_clickid', 'rc_fifo', 'rc_col', 'redirect_log_mongo_id', 'redirect_mongo_id', 'sb_referer_host', 'ss_campaign_id', 'ss_campaign_name', 'ss_campaign_sent_date', 'ss_source', 'trkid', 'ymid', /^trk_.*$/)
+		.DO().REMOVE('_bta_tid', '_bta_c', 'c_id', 'campaign_id', 'cm_cr', 'cm_mmc', 'cm_me', 'cm_re', 'cm_sp', /^cm_mmca\d+$/, 'cmpid', '_cmp', '_ebid', '_ehid', 'ehid', 'ex_cid', 'gdftrk', 'hc_location', 'mbid', '_mid', 'ncid', 'nr_email_referer', 'pmcode', 'rb_clickid', 'rc_fifo', 'rc_col', 'redirect_log_mongo_id', 'redirect_mongo_id', 'sb_referer_host', 'ss_campaign_id', 'ss_campaign_name', 'ss_campaign_sent_date', 'ss_source', 'trkid', 'ymid', /^trk_.*$/)
 ];
