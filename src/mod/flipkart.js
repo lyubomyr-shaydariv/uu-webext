c66ee6504ccbbcd7454c40b844012bf40872c9c6
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-21 19:42:47 +0300

    No query parameters are now retained at Flipkart

--- src/mod/flipkart.js
+++ src/mod/flipkart.js
@@ -1,9 +1,8 @@
-import {PREFIX} from '/literals.js';
 import {RULE} from '/rules.js';
 
 export default [
 	RULE()
 		.AT().DOMAIN('flipkart.com')
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE(PREFIX('affExtParam'), 'affid', /[cilp]id/, 'collection-tab-name', 'fm', 'marketplace', PREFIX('otracker'), /p\[\]/, 'pageUID', 'ppn', 'ppt', 'pwsvid', '_redIf', /sattr\[\]/, 'srno', 'ssid', 'st', 'store')
+		.DO().REMOVE_ALL()
 ];
