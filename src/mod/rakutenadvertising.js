05241d8d53b9fbce544133414b3d2002c6856f75
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-21 14:26:56 +0300

    Improved Rakuten Advertising (LinkShare) support

--- src/mod/rakutenadvertising.js
+++ src/mod/rakutenadvertising.js
@@ -5,5 +5,9 @@ export default [
 		.AT().HOSTNAME('click.linksynergy.com').PATHNAME('/deeplink')
 		.FROM().QUERY_ENTRIES()
 		.APPLY().GET_PROPERTY('murl').TO_URL()
-		.DO().REDIRECT()
+		.DO().REDIRECT(),
+	RULE()
+		.AT().QUERY_ENTRIES_HAVING_ALL_OF('amtlist', 'cur', 'mid', 'ord', 'qlist', 'skulist')
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('amtlist', 'cur', 'mid', 'ord', 'qlist', 'skulist', 'namelist')
 ];
