8bffd09ff63e75d61602f823464f6bdaad326818
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-21 19:48:40 +0300

    Added Lazada support

--- src/mod/lazada.js
+++ src/mod/lazada.js
@@ -2,7 +2,7 @@ import {RULE} from '/rules.js';
 
 export default [
 	RULE()
-		.AT().HOSTNAME(/^(?:[^.]+\.)?lazada\.[^.]+$/)
+		.AT().HOSTNAME(/^(?:[^.]+\.)*lazada(?:\.[^.]+)+$/)
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE('acm', 'ad_src', 'cid', 'clickTrackInfo', 'did', 'laz_trackid', 'mkttid', 'mp', 'pos', 'scm', 'spm', 'trafficFrom')
+		.DO().REMOVE_ALL()
 ];
