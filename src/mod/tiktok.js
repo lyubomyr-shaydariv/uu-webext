9e2c71df3a793b3279d30d31b27979b3384348c1
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-22 00:14:31 +0300

    Improved TikTok support

--- src/mod/tiktok.js
+++ src/mod/tiktok.js
@@ -2,6 +2,10 @@ import { RULE } from '/rules.js';
 
 export default [
 	RULE()
+		.AT().ANYWHERE()
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('ttclid'),
+	RULE()
 		.AT().DOMAIN('tiktok.com')
 		.FROM().QUERY_ENTRIES()
 		.DO().REMOVE('_d', 'checksum', 'embed_source', 'is_copy_url', 'is_from_webapp', 'language', 'preview_pb', 'refer', 'referer_url', 'referer_video_id', 'sec_user_id', 'sender_device', 'sender_web_id', 'share_app_id', 'share_app_name', 'share_iid', 'share_link_id', 'share_item_id', 'source', 'timestamp', 'tt_from', 'u_code', 'user_id'),
