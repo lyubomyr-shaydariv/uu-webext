f2f3200cd5611ebe93d69925bb7a447663660821
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-21 20:37:45 +0300

    No query parameters except `id` and `story_fbid` are now retained at Facebook

--- src/mod/facebook.js
+++ src/mod/facebook.js
@@ -6,9 +6,9 @@ export default [
 		.FROM().QUERY_ENTRY_KEYS()
 		.DO().REMOVE('fbclid', 'fb_action_ids', 'fb_action_types', 'fb_comment_id', 'fb_ref', 'fb_source'),
 	RULE()
-		.AT().DOMAIN('facebook.com')
+		.AT().DOMAIN('facebook.com', 'm.facebook.com')
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE('__cft__[0]', '__cft__%5B0%5D', 'extid', 'fref', 'hrc', 'mibextid', '__tn__', 'refsrc', 'sfnsn', 'tracking'),
+		.DO().RETAIN('id', 'story_fbid'),
 	RULE()
 		.AT().DOMAIN('facebook.com').PATHNAME('/l.php', '/flx/warn/')
 		.FROM().QUERY_ENTRY_KEYS()
