1e18594ecd793f23a7e4363f7904d01f7c9a5101
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-22 01:48:02 +0300

    Improved Ebay support

--- src/mod/ebay.js
+++ src/mod/ebay.js
@@ -2,6 +2,10 @@ import { RULE } from '/rules.js';
 
 export default [
 	RULE()
+		.AT().QUERY_ENTRIES_HAVING_ALL_OF('campid', 'customid', 'mkcid', 'mkevt', 'mkrid', 'toolid')
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('campid', 'customid', 'mkcid', 'mkevt', 'mkrid', 'toolid'),
+	RULE()
 		.AT().HOSTNAME(/^(?:[^.]+\.)?ebay(?:\.[a-z]+)?\.[a-z]+$/)
 		.FROM().QUERY_ENTRIES()
 		.DO().REMOVE('_from', '_trkparms', '_trksid', 'amdata', 'epid', 'hash', 'var'),
