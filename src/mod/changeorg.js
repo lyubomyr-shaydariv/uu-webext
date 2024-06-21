c2f44f10582e97c2813f7c8a1daacd823237cb59
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-21 19:32:05 +0300

    No query parameters are now retained at Change.org

--- src/mod/changeorg.js
+++ src/mod/changeorg.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().DOMAIN('change.org')
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE('guest', 'psf_variant', 'recruited_by_id', 'recruiter', 'share_intent', 'short_display_name', 'source_location')
+		.DO().REMOVE_ALL()
 ];
