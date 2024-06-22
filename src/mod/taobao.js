301c9728249b6851dbebc7417b5b88f3001fd1ce
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-22 09:21:21 +0300

    No query parameters except `id` is now retained at Taobao

--- src/mod/taobao.js
+++ src/mod/taobao.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().DOMAIN('taobao.com')
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE('abbucket', 'abtest', 'acm', 'algo_expid', 'algo_pvid', 'ali_refid', 'ali_trackid', 'app', 'bftRwd', 'bftTag', 'cpp', 'impid', 'lygClk', 'mytmenu', 'ns', 'pos', 'price', 'pvid', 'scene', /^scm[_a-z-]*$/, 'share_crt_v', 'shareurl', 'short_name', 'sourceType', 'sp_tk', 'spm', 'suid', 'trackInfo', 'turing_bucket', '_u', 'un', 'utkn', 'utparam', 'ut_sk')
+		.DO().RETAIN('id')
 ];
