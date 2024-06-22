f8f9566daac084362b8abb3d554207d2eca62afc
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-22 09:17:14 +0300

    No query parameters are now retained at Tiktok

--- src/mod/tiktok.js
+++ src/mod/tiktok.js
@@ -8,7 +8,7 @@ export default [
 	RULE()
 		.AT().DOMAIN('tiktok.com')
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE(/^_[a-z]$/, 'checksum', 'embed_source', 'is_copy_url', 'is_from_webapp', 'language', 'preview_pb', 'refer', 'referer_url', 'referer_video_id', 'sec_u_id', 'sec_user_id', 'sender_device', 'sender_web_id', 'share_app_id', 'share_app_name', 'share_iid', 'share_link_id', 'share_item_id', 'source', 'timestamp', 'tt_from', 'u_code', 'u_id', 'ugbiz_name', 'user_id'),
+		.DO().REMOVE_ALL(),
 	RULE()
 		.AT().DOMAIN('tiktok.com').PATHNAME('/link/v2')
 		.FROM().QUERY_ENTRY_KEYS()
