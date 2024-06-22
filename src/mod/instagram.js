17534494ca97e9f3965c8dcb48ff9617f76cc330
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-22 09:31:13 +0300

    No query parameters are now retained at Instagram

--- src/mod/instagram.js
+++ src/mod/instagram.js
@@ -8,8 +8,7 @@ export default [
 	RULE()
 		.AT().DOMAIN('instagram.com')
 		.FROM().QUERY_ENTRY_KEYS()
-		// TODO is it `saved-by` or `saved_by`?
-		.DO().REMOVE('e', 'g', 'igsh', /^saved[-_]by$/),
+		.DO().REMOVE_ALL(),
 	RULE()
 		.AT().DOMAIN('l.instagram.com')
 		.FROM().QUERY_ENTRY_KEYS()
