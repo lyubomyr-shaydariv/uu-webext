401ddf00c0d5c171463ccf6089cbdd1997113c8d
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-06-22 09:32:51 +0300

    No query parameters are now retained at Netflix

--- src/mod/netflix.js
+++ src/mod/netflix.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().DOMAIN('netflix.com')
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE('clip', 'fromApp', /jb[a-z]*?/, 'netflixsource', 's', 't', 'tctx', 'trackId', 'trg', 'trkid', 'vlang')
+		.DO().REMOVE_ALL()
 ];
