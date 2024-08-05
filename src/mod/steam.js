805f58525d7a1b8826864fab06782be28c69c094
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-08-05 15:58:37 +0300

    Improved Steam support

--- src/mod/steam.js
+++ src/mod/steam.js
@@ -2,6 +2,10 @@ import {RULE} from '/rules.js';
 
 export default [
 	RULE()
+		.AT().DOMAIN('steampowered.com')
+		.FROM().QUERY_ENTRIES()
+		.DO().REMOVE('snr'),
+	RULE()
 		.AT().DOMAIN('steamcommunity.com').PATHNAME('/linkfilter/')
 		.FROM().QUERY_ENTRIES()
 		.APPLY().GET_PROPERTY('url').TO_URL()
