d8503ce6fb145c7c5548ffbb3cee8af74239ff1d
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-08-05 16:01:02 +0300

    Improved TechCrunch support

--- src/mod/techcrunch.js
+++ src/mod/techcrunch.js
@@ -1,8 +1,9 @@
+import {PREFIX} from '/literals.js';
 import {RULE} from '/rules.js';
 
 export default [
 	RULE()
 		.AT().DOMAIN('techcrunch.com')
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('sr', 'sr_share', 'tpcc')
+		.DO().REMOVE('guccounter', PREFIX('guce_referrer_'), 'ncid', 'sr', 'sr_share', 'tpcc')
 ];
