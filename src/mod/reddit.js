5bb470ee324a4719ef4a9fe169806bfeaad7014a
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-08-05 21:21:21 +0300

    Improved Reddit support

--- src/mod/reddit.js
+++ src/mod/reddit.js
@@ -4,7 +4,7 @@ export default [
 	RULE()
 		.AT().DOMAIN('reddit.com')
 		.FROM().QUERY_ENTRIES()
-		.DO().REMOVE('$3p', '$deep_link', '$original_link', 'correlation_id', 'rdt', 'ref_campaign', 'ref_source'),
+		.DO().REMOVE('$3p', '$deep_link', '$original_link', '$original_url', '_branch_match_id', 'correlation_id', 'rdt', 'ref_campaign', 'ref_source'),
 	RULE()
 		.AT().HOSTNAME('out.reddit.com').PATHNAME(/^\/[^/]+$/)
 		.FROM().QUERY_ENTRIES()
