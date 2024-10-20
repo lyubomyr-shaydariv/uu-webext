6d2daf53d2dc88a59d1b424397f6c04db6de9d39
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-10-20 13:16:53 +0300

    [WIP?] https://www.reddit.com/r/NoStupidQuestions/comments/jh0z0r/in_a_url_what_is_scrlybrkr/

--- src/mod/_.js
+++ src/mod/_.js
@@ -4,5 +4,5 @@ export default [
 	RULE()
 		.AT().ANYWHERE()
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE('c_id', 'campaign_id', 'cm_cr', 'cm_mmc', 'cm_me', 'cm_re', 'cm_sp', /^cm_mmca\d+$/, 'cmpid', '_cmp', '_ebid', '_ehid', 'ehid', 'ex_cid', 'hc_location', 'mbid', '_mid', 'ncid', 'nr_email_referer', 'pmcode', 'rb_clickid', 'rc_fifo', 'rc_col', 'sb_referer_host', 'ss_campaign_id', 'ss_campaign_name', 'ss_campaign_sent_date', 'ss_source', 'trkid', 'ymid')
+		.DO().REMOVE('c_id', 'campaign_id', 'cm_cr', 'cm_mmc', 'cm_me', 'cm_re', 'cm_sp', /^cm_mmca\d+$/, 'cmpid', '_cmp', '_ebid', '_ehid', 'ehid', 'ex_cid', 'hc_location', 'mbid', '_mid', 'ncid', 'nr_email_referer', 'pmcode', 'rb_clickid', 'rc_fifo', 'rc_col', 'sb_referer_host', 'scrlybrkr', 'ss_campaign_id', 'ss_campaign_name', 'ss_campaign_sent_date', 'ss_source', 'trkid', 'ymid')
 ];
