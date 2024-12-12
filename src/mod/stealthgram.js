152f721f383f7fb3dd893de99b45b9b1f037c942
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-12-12 21:18:15 +0200

    Improved stealthGram support

--- src/mod/stealthgram.js
+++ src/mod/stealthgram.js
@@ -2,7 +2,7 @@ import {RULE} from '/rules.js';
 
 export default [
 	RULE()
-		.AT().DOMAIN('stealthgram.com').PATHNAME('/download-image/', '/download-video/')
+		.AT().DOMAIN('stealthgram.com').PATHNAME('/api/download-image/', '/api/download-video/', '/download-image/', '/download-video/')
 		.FROM().QUERY_ENTRIES()
 		.APPLY().GET_PROPERTY('url').TO_URL()
 		.DO().REDIRECT()
