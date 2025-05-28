ce6f8dc3f147640869af801135db0b89b94299fb
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2025-05-28 14:22:31 +0300

    Improved AliExpress support

--- src/mod/aliexpress.js
+++ src/mod/aliexpress.js
@@ -9,7 +9,7 @@ export default [
 	RULE()
 		.AT().HOSTNAME(/^(?:[^.]+\.)?aliexpress\.[^.]+$/)
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE('af', 'aff_request_id', 'algo_expid', 'algo_pvid', 'btsid', 'cv', 'dp', 'expid', 'gps-id', 'initiative_id', 'mall_affr', 'scm_id', 'sk', 'terminal_id', 'ws_ab_test'),
+		.DO().REMOVE('af', 'aff_fcid', 'aff_fsk', 'aff_platform', 'aff_request_id', 'aff_short_key', 'aff_trace_key', 'algo_expid', 'algo_pvid', 'btsid', 'curPageLogUid', 'cv', 'dp', 'expid', 'gps-id', 'initiative_id', 'mall_affr', 'pdp_npi', 'pvid', 'scm', 'scm_id', 'scm-url', 'sk', 'spm', 'terminal_id', 'utparam', 'ws_ab_test'),
 	RULE()
 		.AT().HOSTNAME('aliexpress.com').PATHNAME(PREFIX('/item/'))
 		.FROM().QUERY_ENTRY_KEYS()
