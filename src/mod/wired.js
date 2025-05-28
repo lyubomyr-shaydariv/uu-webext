9cb17347a2ff0329a7ab4d208bcbbe301f6bfeaa
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2025-05-28 14:01:24 +0300

    Improved Wired support

--- src/mod/wired.js
+++ src/mod/wired.js
@@ -2,7 +2,7 @@ import {RULE} from '/rules.js';
 
 export default [
 	RULE()
-		.AT().DOMAIN('wired.com')
+		.AT().DOMAIN('wired.com', 'wired.co.uk')
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE('intcid')
+		.DO().REMOVE('intcid', 'mbid')
 ];
