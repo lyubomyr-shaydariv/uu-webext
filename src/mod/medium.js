4d510b5d93c9de18aafcc18660bc134d63ed5cc7
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2025-10-07 22:49:26 +0300

    Improved Medium redirection support

--- src/mod/medium.js
+++ src/mod/medium.js
@@ -6,7 +6,7 @@ export default [
 		.FROM().QUERY_ENTRY_KEYS()
 		.DO().REMOVE('source'),
 	RULE()
-		.AT().DOMAIN('medium.com').PATHNAME('/r/')
+		.AT().DOMAIN('medium.com').PATHNAME('/r', '/r/')
 		.FROM().QUERY_ENTRY_KEYS()
 		.APPLY().GET_PROPERTY('url').TO_URL()
 		.DO().REDIRECT()
