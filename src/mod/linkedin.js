9524bd89a5b1d1fe6998864b2095a24d758378d2
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2026-02-04 11:38:53 +0200

    Improved LinkedIn support

--- src/mod/linkedin.js
+++ src/mod/linkedin.js
@@ -7,23 +7,23 @@ export default [
 		.FROM().QUERY_ENTRY_KEYS()
 		.DO().REMOVE('li_fat_id'),
 	RULE()
-		.AT().DOMAIN('linkedin.com')
+		.AT().DOMAIN('linkedin.com', 'lnkd.in')
 		.FROM().QUERY_ENTRY_KEYS()
 		.DO().REMOVE('eBP', 'eid', 'lgCta', 'lgTemp', /li[a-z]{2}/, 'midSig', 'midToken', 'otpToken', 'recommendedFlavor', 'refId', 'trackingId', 'trk', 'trkEmail'),
 	RULE()
-		.AT().DOMAIN('linkedin.com').PATHNAME('/learning')
+		.AT().DOMAIN('linkedin.com', 'lnkd.in').PATHNAME('/learning')
 		.FROM().QUERY_ENTRY_KEYS()
 		.DO().REMOVE('u'),
 	RULE()
-		.AT().DOMAIN('linkedin.com').PATHNAME(PREFIX('/help/'))
+		.AT().DOMAIN('linkedin.com', 'lnkd.in').PATHNAME(PREFIX('/help/'))
 		.FROM().QUERY_ENTRY_KEYS()
 		.DO().REMOVE('mcid', 'src', 'trk'),
 	RULE()
-		.AT().DOMAIN('linkedin.com').PATHNAME(PREFIX('/in/'))
+		.AT().DOMAIN('linkedin.com', 'lnkd.in').PATHNAME(PREFIX('/in/'))
 		.FROM().QUERY_ENTRY_KEYS()
 		.DO().REMOVE('original_referer', 'trackingCode', 'trackingId'),
 	RULE()
-		.AT().DOMAIN('linkedin.com').PATHNAME('/redir/redirect', '/safety/go')
+		.AT().DOMAIN('linkedin.com', 'lnkd.in').PATHNAME('/redir/redirect', '/safety/go')
 		.FROM().QUERY_ENTRY_KEYS()
 		.APPLY().GET_PROPERTY('url').TO_URL()
 		.DO().REDIRECT()
