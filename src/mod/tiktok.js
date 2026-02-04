02e92683dc7a28173a53a739cfebbe0de39051b8
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2026-02-04 11:42:03 +0200

    Improved TikTok support

--- src/mod/tiktok.js
+++ src/mod/tiktok.js
@@ -8,7 +8,7 @@ export default [
 	RULE()
 		.AT().DOMAIN('tiktok.com')
 		.FROM().QUERY_ENTRY_KEYS()
-		.DO().REMOVE(/^_[a-z]$/, 'checksum', 'embed_source', 'enter_method', 'is_copy_url', 'is_from_webapp', 'language', 'preview_pb', 'q', 'refer', 'referer_url', 'referer_video_id', 'sec_u_id', 'sec_uid', 'sec_user_id', 'sender_device', 'sender_web_id', 'share_app_id', 'share_app_name', 'share_author_id', 'share_iid', 'share_link_id', 'share_item_id', 'source', 't', 'timestamp', 'tt_from', 'u_code', 'u_id', 'ugbiz_name', 'user_id', 'web_id'),
+		.DO().REMOVE(/^_[a-z]$/, 'checksum', 'embed_source', 'enter_method', 'is_copy_url', 'is_from_webapp', 'language', 'preview_pb', 'q', 'refer', 'referer_url', 'referer_video_id', 'sec_u_id', 'sec_uid', 'sec_user_id', 'sender_device', 'sender_web_id', 'share_app_id', 'share_app_name', 'share_author_id', 'share_iid', 'share_link_id', 'share_item_id', 'share_source', 'source', 't', 'timestamp', 'tt_content', 'tt_from', 'u_code', 'u_id', 'ugbiz_name', 'user_id', 'web_id'),
 	RULE()
 		.AT().DOMAIN('tiktok.com').PATHNAME('/link/v2')
 		.FROM().QUERY_ENTRY_KEYS()
