e47cc9a9284ee31647bf4f2eb2aeecb87545ecc5
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2026-09-06 11:13:32 +0300

    Improved Instagram support

--- src/mod/instagram.js
+++ src/mod/instagram.js
@@ -9,7 +9,7 @@ export default [
 		.AT().DOMAIN('instagram.com')
 		.FROM().QUERY_ENTRY_KEYS()
 		// TODO is it `saved-by` or `saved_by`?
-		.DO().REMOVE('e', 'g', 'igsh', /^saved[-_]by$/),
+		.DO().REMOVE('e', 'g', 'igsh', /^saved[-_]by$/, 'stkn'),
 	RULE()
 		.AT().DOMAIN('l.instagram.com')
 		.FROM().QUERY_ENTRY_KEYS()
